7da01f05fa45f3e4cbd95a76195c85807fb2b607
unknown <ntyoussouf@gmail.com>
2016-02-16 19:24:22 +0100

    commit
    rev

--- src/Acme/DemoBundle/Controller/MainController.php
+++ src/Acme/DemoBundle/Controller/MainController.php
@@ -9,7 +9,13 @@ class MainController extends Controller
 {
     public function contactAction()
     {
-        return new Response('<h1>Contactez nous!</h1>');
+        
+        //return new Response('<h1>Contactez nous!</h1>');
         //return $this->render('DemoBundle:Default:index.html.twig', array('name' => $name));
+    }
+    
+    public function helloAction($name)
+    {
+        return new Response('<html><body>'.$name.'!</body></html>');
     }
 }
